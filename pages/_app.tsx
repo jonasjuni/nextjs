0bcc97bc766961071066a9aab2d50bedd262df8b
Jonas Cerqueira da Silva Junior <jonas.junior@trackage.com.br>
2022-05-08 18:45:23 -0300

    env variables test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  console.log('Node Variable:', process.env.TEST_NODE_VARIABLE);
+  console.log('Browser Variable:', process.env.NEXT_PUBLIC_BROWSER_VARIABLE);
+
   return <Component {...pageProps} />
 }
 
