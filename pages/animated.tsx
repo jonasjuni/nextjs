c41b974759f49641d08d673510ed53a29da5cf73
Jonas Cerqueira da Silva Junior <jonas.developer@live.com>
2022-08-15 20:04:12 -0300

    start animation tutorial

--- pages/animated.tsx
+++ pages/animated.tsx
@@ -1,8 +1,21 @@
 import {NextPage} from "next";
+import {Button, Flex, Input, VStack} from "@chakra-ui/react";
 
 const Animated: NextPage = () => {
-    return (<div>
-        Chakra ui is coming
-    </div>)
+    return (
+        <Flex>
+            <VStack spacing="4">
+                <Input/>
+                <Input/>
+                <Button colorScheme="teal" w="full" type="submit">
+                    ADD
+                </Button>
+                <Button colorScheme="red" w="full">
+                    REMOVE
+                </Button>
+            </VStack>
+        </Flex>
+    );
+
 }
 export default Animated;
